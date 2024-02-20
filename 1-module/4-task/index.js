a3d0cc15a13bbd12d36953df27fdf336da99bfa9
m0t0iml <motorbreath6666@gmail.com>
2024-02-20 10:44:06 +0300

    first module complete

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,12 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
+    let upperstr = str.toUpperCase();
+    if ((upperstr.indexOf('1XBET') == -1) && (upperstr.indexOf('XXX') == -1)) {
+        return false;
+    }
+    return true;
 
 }
+
+console.log(checkSpam('fff'));
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,4 +5,13 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
+    let mainstr;
+    let lengthstr = str.length;
+    if (lengthstr > maxlength) {
+        mainstr = str.slice(0, maxlength-1) + "…";
+        return mainstr;
+    }
+    return str;
 }
+
+console.log(truncate('ahrhhaharqeh', 5));
