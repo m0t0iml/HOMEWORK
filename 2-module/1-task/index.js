f8aa97d866bcc944ec83040ca6b34a2360fb8f43
m0t0iml <motorbreath6666@gmail.com>
2024-02-28 15:34:19 +0300

    second module first task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,7 +4,25 @@
  * @returns {Number}
  */
 
-function sumSalary(salaries) {
+let salaries = {
+    John: 1000,
+    Ann: 1600,
+    Pete: 1300,
+    month: 'December',
+    currency: 'USD',
+    isPayed: false
+  }
+
+function sumSalary(obj) {
+    let sum = 0;
+    for (let key in obj) {
+        if (typeof obj[key] == 'number') {
+            sum += obj[key];
+        }
+    }
+    return sum; 
 }
+
+console.log(sumSalary(salaries));
 
 
